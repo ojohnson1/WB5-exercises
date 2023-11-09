bd04782c31eb16418d37fc1fb15dc6bf9e1e6f53
ojohnson1 <ojohnson659@gmail.com>
2023-11-09 14:28:57 -0500

    finished image demo exercise

--- ImagesDemo/scripts/images.js
+++ ImagesDemo/scripts/images.js
@@ -8,4 +8,15 @@ window.onload=function(){
         element.style.border = "1px solid black";
     });
 
+    const allImgs = document.getElementsByTagName("img");
+    Array.from(allImgs).forEach(function (element) {
+        
+        element.className="roundedImg"
+        element.src = "images/space-photo1.png";
+        element.alt = "spacephoto";
+
+    });
+
 }
+
+
